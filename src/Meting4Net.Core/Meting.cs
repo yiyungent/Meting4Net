0d81162a5f017f2f668d56ae29403e452b23acf5
yiyun <yiyungent@gmail.com>
2019-02-15 17:12:51 +0800

    Start

---
+++ src/Meting4Net.Core/Meting.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Meting music framework
+ * https://
+ * https://github.com/yiyungent/Meting4Net
+ * Version 0.1.0
+ *
+ * Copyright 2019, yiyun <yiyungent@gmail.com>
+ * Released under the MIT license
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meting4Net.Core
+{
+    public class Meting
+    {
+
+    }
+}
