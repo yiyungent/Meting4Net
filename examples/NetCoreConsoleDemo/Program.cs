8ac352512765ae9df89efd93ccfe83914490d172
yiyun <yiyungent@gmail.com>
2019-02-20 12:53:36 +0800

    Add .netcore console demo

---
+++ examples/NetCoreConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Meting4Net.Core;
+
+namespace NetCoreConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meting api = new Meting("tencent");
+            string jsonStr = api.FormatMethod(true).Search("千里邀月");
+            Console.WriteLine(jsonStr);
+
+            Console.ReadLine();
+        }
+    }
+}
