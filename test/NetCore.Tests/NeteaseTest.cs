551850910031265c5ad4e56e23d40985b4d04d26
yiyun <yiyungent@gmail.com>
2019-02-24 23:03:36 +0800

    update NeteaseTest

--- test/NetCore.Tests/NeteaseTest.cs
+++ test/NetCore.Tests/NeteaseTest.cs
@@ -3,12 +3,17 @@ using Xunit;
 
 using Meting4Net.Core;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace NetCore.Tests
 {
     public class NeteaseTest
     {
         public Meting Api { get; set; }
+
+        public int TryCount { get; set; } = 5;
+
+        public int SleepMsec { get; set; } = 20000;
 
         public NeteaseTest()
         {
@@ -18,104 +23,205 @@ namespace NetCore.Tests
         [Fact]
         public void SearchTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Search("千里邀月");
+            bool isPass = false;
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Search("千里邀月", new Meting4Net.Core.Models.Standard.Options
+                {
+                    limit = 3,
+                    page = 1
+                });
+                if (jsonStr.Contains("千里邀月"))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("千里邀月", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void UrlTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Url("35847388");
+            bool isPass = false;
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Url("35847388");
+                if (jsonStr.Contains("url") && jsonStr.Contains("size") && jsonStr.Contains("br"))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("\"url\":", jsonStr);
-            Assert.Contains("\"size\":", jsonStr);
-            Assert.Contains("\"br\":", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void SongTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Song("35847388");
+            bool isPass = false;
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                if (jsonStr.Contains("id") && jsonStr.Contains("name") && jsonStr.Contains("artist") && jsonStr.Contains("album") && jsonStr.Contains("pic_id") && jsonStr.Contains("url_id") && jsonStr.Contains("lyric_id") && jsonStr.Contains("source"))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("id", jsonStr);
-            Assert.Contains("name", jsonStr);
-            Assert.Contains("artist", jsonStr);
-            Assert.Contains("album", jsonStr);
-            Assert.Contains("pic_id", jsonStr);
-            Assert.Contains("url_id", jsonStr);
-            Assert.Contains("lyric_id", jsonStr);
-            Assert.Contains("source", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void AlbumTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Album("73927024");
+            bool isPass = false;
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Album("73927024");
+                isPass = jsonStr.Contains("id") && jsonStr.Contains("name") && jsonStr.Contains("artist") && jsonStr.Contains("album") && jsonStr.Contains("pic_id") && jsonStr.Contains("url_id") && jsonStr.Contains("lyric_id") && jsonStr.Contains("source");
+                if (isPass)
+                {
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("id", jsonStr);
-            Assert.Contains("name", jsonStr);
-            Assert.Contains("artist", jsonStr);
-            Assert.Contains("album", jsonStr);
-            Assert.Contains("pic_id", jsonStr);
-            Assert.Contains("url_id", jsonStr);
-            Assert.Contains("lyric_id", jsonStr);
-            Assert.Contains("source", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void ArtistTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Artist("1049179");
+            bool isPass = false;
+            string[] subStrArr =
+            {
+                "id",
+                "name",
+                "artist",
+                "album",
+                "pic_id",
+                "url_id",
+                "lyric_id",
+                "source",
+            };
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Artist("1049179");
+                if (ContainsStrArr(jsonStr, subStrArr))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("id", jsonStr);
-            Assert.Contains("name", jsonStr);
-            Assert.Contains("artist", jsonStr);
-            Assert.Contains("album", jsonStr);
-            Assert.Contains("pic_id", jsonStr);
-            Assert.Contains("url_id", jsonStr);
-            Assert.Contains("lyric_id", jsonStr);
-            Assert.Contains("source", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void PlaylistTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Playlist("2487120533");
+            bool isPass = false;
+            string[] subStrArr =
+            {
+                "id",
+                "name",
+                "artist",
+                "album",
+                "pic_id",
+                "url_id",
+                "lyric_id",
+                "source",
+            };
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Playlist("2487120533");
+                if (ContainsStrArr(jsonStr, subStrArr))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("id", jsonStr);
-            Assert.Contains("name", jsonStr);
-            Assert.Contains("artist", jsonStr);
-            Assert.Contains("album", jsonStr);
-            Assert.Contains("pic_id", jsonStr);
-            Assert.Contains("url_id", jsonStr);
-            Assert.Contains("lyric_id", jsonStr);
-            Assert.Contains("source", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void LyricTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Lyric("35847388");
+            bool isPass = false;
+            string[] subStrArr =
+            {
+               "lyric",
+                "tlyric",
+            };
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Lyric("35847388");
+                if (ContainsStrArr(jsonStr, subStrArr))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("lyric", jsonStr);
-            Assert.Contains("tlyric", jsonStr);
+            Assert.True(isPass);
         }
 
         [Fact]
         public void PicTest()
         {
-            Meting api = new Meting(ServerProvider.Netease);
-            string jsonStr = Api.FormatMethod(true).Pic("1407374890649284");
+            bool isPass = false;
+            string[] subStrArr =
+            {
+               "url"
+            };
+            string jsonStr = string.Empty;
+            for (int i = 0; i < TryCount; i++)
+            {
+                jsonStr = Api.FormatMethod(true).Pic("1407374890649284");
+                if (ContainsStrArr(jsonStr, subStrArr))
+                {
+                    isPass = true;
+                    break;
+                }
+                Thread.Sleep(SleepMsec * (i + 1));
+            }
 
-            Assert.Contains("url", jsonStr);
+            Assert.True(isPass);
         }
+
+        #region 指定的字符串是否包括全部的子串数组
+        private bool ContainsStrArr(string searchStr, string[] subStrArr)
+        {
+            bool isAllContain = true;
+            foreach (string subStr in subStrArr)
+            {
+                isAllContain = searchStr.Contains(subStr);
+                // 主要有一个不包含，则退出，标记为不包含false
+                if (!isAllContain)
+                {
+                    break;
+                }
+            }
+
+            return isAllContain;
+        }
+        #endregion
     }
 }
